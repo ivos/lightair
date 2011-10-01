efa5e5fb456b9960552bc033ed77d3634ce9ea0e
Ivo Maixner <ivo.maixner@gmail.com>
2011-10-01 11:54:01 +0200

    Add documentation to verify annotation i.tests.

--- src/test/java/it/verify/annotation/DefaultClassVerifyTest.java
+++ src/test/java/it/verify/annotation/DefaultClassVerifyTest.java
@@ -4,14 +4,23 @@ import net.sf.lightair.annotation.Verify;
 
 import org.junit.Test;
 
+/**
+ * Test class is annotated @Verify.
+ */
 @Verify
 public class DefaultClassVerifyTest extends VerifyTestBase {
 
+	/**
+	 * Verifies '&lt;test class name>-verify.xml'.
+	 */
 	@Test
 	public void classNameXml() {
 		fillPersons(2);
 	}
 
+	/**
+	 * Verifies '&lt;test class name>.&lt;test method name>-verify.xml'.
+	 */
 	@Test
 	public void classNameMethodNameXml() {
 		fillPersons(3);
--- src/test/java/it/verify/annotation/DefaultMethodVerifyTest.java
+++ src/test/java/it/verify/annotation/DefaultMethodVerifyTest.java
@@ -4,14 +4,23 @@ import net.sf.lightair.annotation.Verify;
 
 import org.junit.Test;
 
+/**
+ * Test class is not annotated. Test methods are annotated @Verify.
+ */
 public class DefaultMethodVerifyTest extends VerifyTestBase {
 
+	/**
+	 * Verifies '&lt;test class name>-verify.xml'.
+	 */
 	@Test
 	@Verify
 	public void classNameXml() {
 		fillPersons(2);
 	}
 
+	/**
+	 * Verifies '&lt;test class name>.&lt;test method name>-verify.xml'.
+	 */
 	@Test
 	@Verify
 	public void classNameMethodNameXml() {
