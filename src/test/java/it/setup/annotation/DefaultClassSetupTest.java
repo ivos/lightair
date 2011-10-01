6fb771bc1eebd10986896a01b7739223a02c5af3
Ivo Maixner <ivo.maixner@gmail.com>
2011-10-01 11:47:43 +0200

    Add documentation to setup annotation i.tests.

--- src/test/java/it/setup/annotation/DefaultClassSetupTest.java
+++ src/test/java/it/setup/annotation/DefaultClassSetupTest.java
@@ -4,14 +4,23 @@ import net.sf.lightair.annotation.Setup;
 
 import org.junit.Test;
 
+/**
+ * Test class is annotated @Setup.
+ */
 @Setup
 public class DefaultClassSetupTest extends SetupTestBase {
 
+	/**
+	 * Loads setup '&lt;test class name>.xml'.
+	 */
 	@Test
 	public void classNameXml() {
 		verifyPersons(2);
 	}
 
+	/**
+	 * Loads setup '&lt;test class name>.&lt;test method name>.xml'.
+	 */
 	@Test
 	public void classNameMethodNameXml() {
 		verifyPersons(3);
--- src/test/java/it/setup/annotation/DefaultMethodSetupTest.java
+++ src/test/java/it/setup/annotation/DefaultMethodSetupTest.java
@@ -4,14 +4,23 @@ import net.sf.lightair.annotation.Setup;
 
 import org.junit.Test;
 
+/**
+ * Test class is not annotated. Test methods are annotated @Setup.
+ */
 public class DefaultMethodSetupTest extends SetupTestBase {
 
+	/**
+	 * Loads setup '&lt;test class name>.xml'.
+	 */
 	@Test
 	@Setup
 	public void classNameXml() {
 		verifyPersons(2);
 	}
 
+	/**
+	 * Loads setup '&lt;test class name>.&lt;test method name>.xml'.
+	 */
 	@Test
 	@Setup
 	public void classNameMethodNameXml() {
