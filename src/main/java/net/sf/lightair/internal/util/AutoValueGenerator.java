2731584920292615f28c0021f9d53d42e6d337ce
Ivo Maixner <ivo.maixner@gmail.com>
2013-12-12 20:19:56 +0100

    ADD support for remaining data types, switch to return String.

--- src/main/java/net/sf/lightair/internal/util/AutoValueGenerator.java
+++ src/main/java/net/sf/lightair/internal/util/AutoValueGenerator.java
@@ -1,42 +1,75 @@
 package net.sf.lightair.internal.util;
 
-import java.sql.Date;
 import java.sql.Time;
+import java.sql.Timestamp;
 import java.sql.Types;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import net.sf.lightair.exception.UnsupportedDataType;
 
 import org.apache.commons.lang.StringUtils;
 import org.dbunit.dataset.datatype.DataType;
-import org.joda.time.DateTimeZone;
 import org.joda.time.LocalDate;
-import org.joda.time.LocalTime;
+import org.joda.time.LocalDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AutoValueGenerator {
 
+	private final Logger log = LoggerFactory
+			.getLogger(AutoValueGenerator.class);
+
 	private AutoNumberGenerator autoNumberGenerator;
 
-	public Object generateAutoValue(DataType dataType, String tableName,
+	public String generateAutoValue(DataType dataType, String tableName,
 			String columnName) {
-		final int autoNumber = autoNumberGenerator.generateAutoNumber(
-				tableName, columnName, 0);
-		final String autoNumberString = StringUtils.leftPad(
+		int autoNumber = autoNumberGenerator.generateAutoNumber(tableName,
+				columnName, 0);
+		String value = generate(dataType, columnName, autoNumber);
+		log.debug("Generated auto value for {}.{} data type {} as {}.",
+				tableName, columnName, dataType, value);
+		return value;
+	}
+
+	private String generate(DataType dataType, String columnName, int autoNumber) {
+		String autoNumberString = StringUtils.leftPad(
 				String.valueOf(autoNumber), 7, '0');
+		int offset = new GregorianCalendar().get(Calendar.ZONE_OFFSET);
+
 		switch (dataType.getSqlType()) {
 		case Types.INTEGER:
-			return new Integer(autoNumber);
+		case Types.BIGINT:
+		case Types.DECIMAL:
+			return String.valueOf(autoNumber);
+		case Types.DOUBLE:
+			return String.valueOf(((double) autoNumber) / 100);
+		case Types.BOOLEAN:
+			return String.valueOf(0 != autoNumber % 2);
 		case Types.CHAR:
 		case Types.VARCHAR:
+		case Types.CLOB:
+		case Types.BLOB:
+		case Types.VARBINARY:
 			return columnName + ' ' + autoNumberString;
 		case Types.DATE:
-			// add autoNumber days from 1900-01-01, wrapping around 2100
-			// date from 1900-01-01 to 2099-11-12
-			return new Date(new LocalDate(1900, 1, 1)
-					.plusDays(autoNumber % 73000).toDate().getTime());
+			// Add autoNumber days from local date 1900-01-01, wrapping
+			// approximately around 2100.
+			// Date from 1900-01-01 to 2099-11-12
+			return new LocalDate(1900, 1, 1).plusDays(autoNumber % 73000)
+					.toString();
 		case Types.TIME:
-			// add autoNumber of seconds
-			return new Time(new LocalTime(1000L * autoNumber,
-					DateTimeZone.getDefault()).getMillisOfDay());
+			// Add autoNumber of seconds to local time 0:00:00.
+			return new Time(1000L * autoNumber - offset).toString();
+		case Types.TIMESTAMP:
+			// Put autoNumber twice after each other.
+			// Add resulting number of milliseconds from local date 1900-01-01,
+			// wrapping approximately around 2100.
+			return new Timestamp(new LocalDateTime(1900, 1, 1, 0, 0, 0)
+					.plusDays(autoNumber % 73000)
+					.plusSeconds(autoNumber % 86400)
+					.plusMillis(autoNumber % 1000).toDate().getTime())
+					.toString();
 		}
 		throw new UnsupportedDataType(dataType.toString());
 	}
--- src/test/java/unit/internal/util/AutoValueGeneratorTest.java
+++ src/test/java/unit/internal/util/AutoValueGeneratorTest.java
@@ -1,10 +1,6 @@
 package unit.internal.util;
 
 import static org.junit.Assert.*;
-
-import java.sql.Date;
-import java.sql.Time;
-
 import net.sf.lightair.internal.util.AutoNumberGenerator;
 import net.sf.lightair.internal.util.AutoValueGenerator;
 import net.sf.seaf.test.jmock.JMockSupport;
@@ -39,29 +35,29 @@ public class AutoValueGeneratorTest extends JMockSupport {
 	@Test
 	public void integer_0_0() {
 		check(0, 0);
-		assertEquals(0, ((Integer) g.generateAutoValue(DataType.INTEGER,
-				"tableName1", "columnName1")).intValue());
+		assertEquals("0", g.generateAutoValue(DataType.INTEGER, "tableName1",
+				"columnName1"));
 	}
 
 	@Test
 	public void integer_0_1() {
 		check(0, 1);
-		assertEquals(1, ((Integer) g.generateAutoValue(DataType.INTEGER,
-				"tableName1", "columnName1")).intValue());
+		assertEquals("1", g.generateAutoValue(DataType.INTEGER, "tableName1",
+				"columnName1"));
 	}
 
 	@Test
 	public void integer_0_9999999() {
 		check(0, 9999999);
-		assertEquals(9999999, ((Integer) g.generateAutoValue(DataType.INTEGER,
-				"tableName1", "columnName1")).intValue());
+		assertEquals("9999999", g.generateAutoValue(DataType.INTEGER,
+				"tableName1", "columnName1"));
 	}
 
 	@Test
 	public void integer_0_1234567() {
 		check(0, 1234567);
-		assertEquals(1234567, ((Integer) g.generateAutoValue(DataType.INTEGER,
-				"tableName1", "columnName1")).intValue());
+		assertEquals("1234567", g.generateAutoValue(DataType.INTEGER,
+				"tableName1", "columnName1"));
 	}
 
 	@Test
@@ -93,93 +89,317 @@ public class AutoValueGeneratorTest extends JMockSupport {
 	}
 
 	@Test
+	public void clob_0_1234567() {
+		check(0, 1234567);
+		assertEquals("columnName1 1234567",
+				g.generateAutoValue(DataType.CLOB, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void blob_0_1234567() {
+		check(0, 1234567);
+		assertEquals("columnName1 1234567",
+				g.generateAutoValue(DataType.BLOB, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void varbinary_0_1234567() {
+		check(0, 1234567);
+		assertEquals("columnName1 1234567", g.generateAutoValue(
+				DataType.VARBINARY, "tableName1", "columnName1"));
+	}
+
+	@Test
 	public void date_0_0() {
 		check(0, 0);
-		final Object actual = g.generateAutoValue(DataType.DATE, "tableName1",
-				"columnName1");
-		assertEquals(Date.class, actual.getClass());
-		assertEquals("1900-01-01", actual.toString());
+		assertEquals("1900-01-01",
+				g.generateAutoValue(DataType.DATE, "tableName1", "columnName1"));
 	}
 
 	@Test
 	public void date_0_1() {
 		check(0, 1);
-		final Object actual = g.generateAutoValue(DataType.DATE, "tableName1",
-				"columnName1");
-		assertEquals(Date.class, actual.getClass());
-		assertEquals("1900-01-02", actual.toString());
+		assertEquals("1900-01-02",
+				g.generateAutoValue(DataType.DATE, "tableName1", "columnName1"));
 	}
 
 	@Test
 	public void date_0_67891() {
 		check(0, 67891);
-		final Object actual = g.generateAutoValue(DataType.DATE, "tableName1",
-				"columnName1");
-		assertEquals(Date.class, actual.getClass());
-		assertEquals("2085-11-17", actual.toString());
+		assertEquals("2085-11-17",
+				g.generateAutoValue(DataType.DATE, "tableName1", "columnName1"));
 	}
 
 	@Test
 	public void date_0_72998() {
 		check(0, 72998);
-		final Object actual = g.generateAutoValue(DataType.DATE, "tableName1",
-				"columnName1");
-		assertEquals(Date.class, actual.getClass());
-		assertEquals("2099-11-11", actual.toString());
+		assertEquals("2099-11-11",
+				g.generateAutoValue(DataType.DATE, "tableName1", "columnName1"));
 	}
 
 	@Test
 	public void date_0_72999() {
 		check(0, 72999);
-		final Object actual = g.generateAutoValue(DataType.DATE, "tableName1",
-				"columnName1");
-		assertEquals(Date.class, actual.getClass());
-		assertEquals("2099-11-12", actual.toString());
+		assertEquals("2099-11-12",
+				g.generateAutoValue(DataType.DATE, "tableName1", "columnName1"));
 	}
 
 	@Test
 	public void date_0_73000() {
 		check(0, 73000);
-		final Object actual = g.generateAutoValue(DataType.DATE, "tableName1",
-				"columnName1");
-		assertEquals(Date.class, actual.getClass());
-		assertEquals("1900-01-01", actual.toString());
+		assertEquals("1900-01-01",
+				g.generateAutoValue(DataType.DATE, "tableName1", "columnName1"));
 	}
 
 	@Test
 	public void date_0_73001() {
 		check(0, 73001);
-		final Object actual = g.generateAutoValue(DataType.DATE, "tableName1",
-				"columnName1");
-		assertEquals(Date.class, actual.getClass());
-		assertEquals("1900-01-02", actual.toString());
+		assertEquals("1900-01-02",
+				g.generateAutoValue(DataType.DATE, "tableName1", "columnName1"));
 	}
 
 	@Test
 	public void date_0_9999999() {
 		check(0, 9999999);
-		final Object actual = g.generateAutoValue(DataType.DATE, "tableName1",
-				"columnName1");
-		assertEquals(Date.class, actual.getClass());
-		assertEquals("2097-02-15", actual.toString());
+		assertEquals("2097-02-15",
+				g.generateAutoValue(DataType.DATE, "tableName1", "columnName1"));
 	}
 
 	@Test
 	public void time_0_0() {
 		check(0, 0);
-		final Object actual = g.generateAutoValue(DataType.TIME, "tableName1",
-				"columnName1");
-		assertEquals(Time.class, actual.getClass());
-		assertEquals("00:00:00", actual.toString());
+		assertEquals("00:00:00",
+				g.generateAutoValue(DataType.TIME, "tableName1", "columnName1"));
 	}
 
 	@Test
 	public void time_0_1() {
 		check(0, 1);
-		final Object actual = g.generateAutoValue(DataType.TIME, "tableName1",
-				"columnName1");
-		assertEquals(Time.class, actual.getClass());
-		assertEquals("00:00:00", actual.toString());
+		assertEquals("00:00:01",
+				g.generateAutoValue(DataType.TIME, "tableName1", "columnName1"));
 	}
 
+	@Test
+	public void time_0_86399() {
+		check(0, 86399);
+		assertEquals("23:59:59",
+				g.generateAutoValue(DataType.TIME, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void time_0_86400() {
+		check(0, 86400);
+		assertEquals("00:00:00",
+				g.generateAutoValue(DataType.TIME, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void time_0_86401() {
+		check(0, 86401);
+		assertEquals("00:00:01",
+				g.generateAutoValue(DataType.TIME, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void timestamp_0_0() {
+		check(0, 0);
+		assertEquals("1900-01-01 00:00:00.0", g.generateAutoValue(
+				DataType.TIMESTAMP, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void timestamp_0_1() {
+		check(0, 1);
+		assertEquals("1900-01-02 00:00:01.001", g.generateAutoValue(
+				DataType.TIMESTAMP, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void timestamp_0_2() {
+		check(0, 2);
+		assertEquals("1900-01-03 00:00:02.002", g.generateAutoValue(
+				DataType.TIMESTAMP, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void timestamp_0_10() {
+		check(0, 10);
+		assertEquals("1900-01-11 00:00:10.01", g.generateAutoValue(
+				DataType.TIMESTAMP, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void timestamp_0_100() {
+		check(0, 100);
+		assertEquals("1900-04-11 00:01:40.1", g.generateAutoValue(
+				DataType.TIMESTAMP, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void timestamp_0_999() {
+		check(0, 999);
+		assertEquals("1902-09-27 00:16:39.999", g.generateAutoValue(
+				DataType.TIMESTAMP, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void timestamp_0_1000() {
+		check(0, 1000);
+		assertEquals("1902-09-28 00:16:40.0", g.generateAutoValue(
+				DataType.TIMESTAMP, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void timestamp_0_72999() {
+		check(0, 72999);
+		assertEquals("2099-11-12 20:16:39.999", g.generateAutoValue(
+				DataType.TIMESTAMP, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void timestamp_0_73000() {
+		check(0, 73000);
+		assertEquals("1900-01-01 20:16:40.0", g.generateAutoValue(
+				DataType.TIMESTAMP, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void timestamp_0_86399() {
+		check(0, 86399);
+		assertEquals("1936-09-08 23:59:59.399", g.generateAutoValue(
+				DataType.TIMESTAMP, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void timestamp_0_86400() {
+		check(0, 86400);
+		assertEquals("1936-09-09 00:00:00.4", g.generateAutoValue(
+				DataType.TIMESTAMP, "tableName1", "columnName1"));
+	}
+
+	@Test
+	public void double_0_0() {
+		check(0, 0);
+		assertEquals("0.0", g.generateAutoValue(DataType.DOUBLE, "tableName1",
+				"columnName1"));
+	}
+
+	@Test
+	public void double_0_1() {
+		check(0, 1);
+		assertEquals("0.01", g.generateAutoValue(DataType.DOUBLE, "tableName1",
+				"columnName1"));
+	}
+
+	@Test
+	public void double_0_99() {
+		check(0, 99);
+		assertEquals("0.99", g.generateAutoValue(DataType.DOUBLE, "tableName1",
+				"columnName1"));
+	}
+
+	@Test
+	public void double_0_100() {
+		check(0, 100);
+		assertEquals("1.0", g.generateAutoValue(DataType.DOUBLE, "tableName1",
+				"columnName1"));
+	}
+
+	@Test
+	public void double_0_101() {
+		check(0, 101);
+		assertEquals("1.01", g.generateAutoValue(DataType.DOUBLE, "tableName1",
+				"columnName1"));
+	}
+
+	@Test
+	public void double_0_9999999() {
+		check(0, 9999999);
+		assertEquals("99999.99", g.generateAutoValue(DataType.DOUBLE,
+				"tableName1", "columnName1"));
+	}
+
+	@Test
+	public void boolean_0_0() {
+		check(0, 0);
+		assertEquals("false", g.generateAutoValue(DataType.BOOLEAN,
+				"tableName1", "columnName1"));
+	}
+
+	@Test
+	public void boolean_0_1() {
+		check(0, 1);
+		assertEquals("true", g.generateAutoValue(DataType.BOOLEAN,
+				"tableName1", "columnName1"));
+	}
+
+	@Test
+	public void boolean_0_2() {
+		check(0, 2);
+		assertEquals("false", g.generateAutoValue(DataType.BOOLEAN,
+				"tableName1", "columnName1"));
+	}
+
+	@Test
+	public void boolean_0_3() {
+		check(0, 3);
+		assertEquals("true", g.generateAutoValue(DataType.BOOLEAN,
+				"tableName1", "columnName1"));
+	}
+
+	@Test
+	public void boolean_0_9999998() {
+		check(0, 9999998);
+		assertEquals("false", g.generateAutoValue(DataType.BOOLEAN,
+				"tableName1", "columnName1"));
+	}
+
+	@Test
+	public void boolean_0_9999999() {
+		check(0, 9999999);
+		assertEquals("true", g.generateAutoValue(DataType.BOOLEAN,
+				"tableName1", "columnName1"));
+	}
+
+	@Test
+	public void bigint_0_0() {
+		check(0, 0);
+		assertEquals("0", g.generateAutoValue(DataType.BIGINT, "tableName1",
+				"columnName1"));
+	}
+
+	@Test
+	public void bigint_0_1() {
+		check(0, 1);
+		assertEquals("1", g.generateAutoValue(DataType.BIGINT, "tableName1",
+				"columnName1"));
+	}
+
+	@Test
+	public void bigint_0_9999999() {
+		check(0, 9999999);
+		assertEquals("9999999", g.generateAutoValue(DataType.BIGINT,
+				"tableName1", "columnName1"));
+	}
+
+	@Test
+	public void decimal_0_0() {
+		check(0, 0);
+		assertEquals("0", g.generateAutoValue(DataType.DECIMAL, "tableName1",
+				"columnName1"));
+	}
+
+	@Test
+	public void decimal_0_1() {
+		check(0, 1);
+		assertEquals("1", g.generateAutoValue(DataType.DECIMAL, "tableName1",
+				"columnName1"));
+	}
+
+	@Test
+	public void decimal_0_9999999() {
+		check(0, 9999999);
+		assertEquals("9999999", g.generateAutoValue(DataType.DECIMAL,
+				"tableName1", "columnName1"));
+	}
 }
