b4a4ff53d139efc79a9bb13075d4744842144d5d
Ivo Maixner <ivo.maixner@gmail.com>
2016-07-18 13:44:18 +0200

    Fix logging.

--- src/main/java/net/sf/lightair/internal/Xml.java
+++ src/main/java/net/sf/lightair/internal/Xml.java
@@ -1,5 +1,7 @@
 package net.sf.lightair.internal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
@@ -21,12 +23,15 @@ import java.util.Map;
 
 public class Xml implements Keywords {
 
+	private static final Logger log = LoggerFactory.getLogger(Xml.class);
+
 	public static Map<String, List<Map<String, Object>>> read(Map<String, List<String>> fileNames) {
 		Map<String, List<Map<String, Object>>> datasets = new LinkedHashMap<>();
 		for (String profile : fileNames.keySet()) {
 			List<Map<String, Object>> profileDataset = new ArrayList<>();
 			List<String> profileFileNames = fileNames.get(profile);
 			for (String fileName : profileFileNames) {
+				log.debug("For profile [{}] reading XML file {}.", profile, fileName);
 				List<Map<String, Object>> fileDataset = readFile(new File(fileName));
 				profileDataset.addAll(fileDataset);
 			}
