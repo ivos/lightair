518ed6855e33037275957e3cf23f1e61f04e1db5
Ivo Maixner <ivo.maixner@gmail.com>
2016-08-04 14:25:54 +0200

    Switch derby tests into custom H2 schema tests. Remove multischema test.

--- src/test/java/it/verify/core/ProfilesTest.java
+++ src/test/java/it/verify/core/ProfilesTest.java
@@ -11,8 +11,7 @@ import org.junit.runner.RunWith;
 @Verify.List({
 		@Verify("../../common/profiles-defaulth2.xml"),
 		@Verify(value = "../../common/profiles-hsqldb.xml", profile = "hsqldb"),
-		@Verify(value = { "../../common/profiles-derby1.xml",
-				"../../common/profiles-derby2.xml" }, profile = "derby") })
+		@Verify(value = { "../../common/profiles-custom1.xml", "../../common/profiles-custom2.xml" }, profile = "custom") })
 public class ProfilesTest extends ProfilesTestBase {
 
 	@Test
