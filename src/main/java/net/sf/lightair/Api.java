4e3b218101d5551c0a545031fb6bc3d7887d3046
Ivo Maixner <ivo.maixner@gmail.com>
2016-09-19 15:29:16 +0200

    Add API docs. Minor fixes to site docs.

--- src/main/java/net/sf/lightair/Api.java
+++ src/main/java/net/sf/lightair/Api.java
@@ -25,6 +25,33 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Light Air's programmatic API.
+ * <p>
+ * The general contract is to {@link Api#initialize(String)} and {@link Api#shutdown()}
+ * and in between perform either {@link Api#generateXsd()} call
+ * or (a series of) {@link Api#setup(Map)} and {@link Api#verify(Map)} calls.
+ * <p>
+ * To generate XSD files:
+ * <pre>
+ * Api.initialize(Api.getPropertiesFileName());
+ * Api.generateXsd();
+ * Api.shutdown();
+ * </pre>
+ * <p>
+ * To setup and verify database:
+ * <pre>
+ * Map&lt;String, List&lt;String&gt;&gt; fileNames = new HashMap&lt;&gt;();
+ * fileNames.put(Keywords.DEFAULT_PROFILE, Arrays.asList("my-dataset-1.xml", "my-dataset-2.xml"));
+ *
+ * Api.initialize(Api.getPropertiesFileName());
+ * Api.setup(fileNames);
+ * // ...execute function under test...
+ * Api.verify(fileNames);
+ * // ...possibly repeat setup and verify for other tests...
+ * Api.shutdown();
+ * </pre>
+ */
 public class Api implements Keywords {
 
 	private static final Logger log = LoggerFactory.getLogger(Api.class);
@@ -37,6 +64,11 @@ public class Api implements Keywords {
 	private static Map<String, Map<String, Map<String, Map<String, Object>>>> structures;
 	private static Map<String, String> index;
 
+	/**
+	 * Get the file name of the main properties file.
+	 *
+	 * @return file name
+	 */
 	public static String getPropertiesFileName() {
 		String fileName = System.getProperty(PROPERTIES_PROPERTY_NAME);
 		if (null == fileName) {
@@ -45,6 +77,13 @@ public class Api implements Keywords {
 		return fileName;
 	}
 
+	/**
+	 * Initialize Light Air.
+	 * <p>
+	 * NOTE: ALWAYS call {@link Api#shutdown()} after initialize() to properly release resources.
+	 *
+	 * @param propertiesFileName file name of the main Light Air properties
+	 */
 	public static void initialize(String propertiesFileName) {
 		log.info("Initializing Light Air.");
 		Api.propertiesFileName = propertiesFileName;
@@ -52,12 +91,20 @@ public class Api implements Keywords {
 		log.debug("Light Air has initialized.");
 	}
 
+	/**
+	 * Shutdown initialized Light Air to properly release resources, like DB connections.
+	 */
 	public static void shutdown() {
 		log.debug("Shutting down Light Air.");
 		performShutdown();
 		log.info("Light Air has shut down.");
 	}
 
+	/**
+	 * Re-initialize already initialized Light Air.
+	 * <p>
+	 * Reloads the properties file(s) and cleanly re-initializes Light Air.
+	 */
 	public static void reInitialize() {
 		log.info("Re-initializing Light Air.");
 		performShutdown();
@@ -65,6 +112,11 @@ public class Api implements Keywords {
 		log.debug("Light Air has re-initialized.");
 	}
 
+	/**
+	 * Initialize Light Air if it is not initialized already.
+	 *
+	 * @param propertiesFileName file name of the main Light Air properties
+	 */
 	public static void ensureInitialized(String propertiesFileName) {
 		log.trace("Ensuring Light Air has been initialized.");
 		if (null == index) {
@@ -72,6 +124,9 @@ public class Api implements Keywords {
 		}
 	}
 
+	/**
+	 * Shutdown Light Air to properly release resources, like DB connections, if it is not shutdown already.
+	 */
 	public static void ensureShutdown() {
 		log.trace("Ensuring Light Air has been shutdown.");
 		if (null != properties) {
@@ -94,10 +149,18 @@ public class Api implements Keywords {
 		properties = null;
 	}
 
+	/**
+	 * Generate XSD files from the database structure.
+	 */
 	public static void generateXsd() {
 		Xsd.generate(properties, structures);
 	}
 
+	/**
+	 * Setup database.
+	 *
+	 * @param fileNames map of profile names to lists of names of setup dataset files for the profile
+	 */
 	public static void setup(Map<String, List<String>> fileNames) {
 		log.info("Performing setup for files {}.", fileNames);
 		Map<String, List<Map<String, Object>>> xmlDatasets = Xml.read(fileNames);
@@ -118,6 +181,11 @@ public class Api implements Keywords {
 		log.debug("Finished setup for files {}.", fileNames);
 	}
 
+	/**
+	 * Verify database.
+	 *
+	 * @param fileNames map of profile names to lists of names of verification dataset files for the profile
+	 */
 	public static void verify(Map<String, List<String>> fileNames) {
 		log.info("Performing verify for files {}.", fileNames);
 		Map<String, List<Map<String, Object>>> xmlDatasets = Xml.read(fileNames);
